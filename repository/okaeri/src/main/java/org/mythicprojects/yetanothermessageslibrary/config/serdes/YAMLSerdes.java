6c0825fa45e963fd7c70497228c54901bf723daf
Peridot <peridot491@protonmail.com>
2024-08-08 23:54:53 +0200

    Convert ComponentTransformer to ComponentSerializer to allow serialzation of any Component type

--- repository/okaeri/src/main/java/org/mythicprojects/yetanothermessageslibrary/config/serdes/YAMLSerdes.java
+++ repository/okaeri/src/main/java/org/mythicprojects/yetanothermessageslibrary/config/serdes/YAMLSerdes.java
@@ -3,7 +3,6 @@ package org.mythicprojects.yetanothermessageslibrary.config.serdes;
 import eu.okaeri.configs.serdes.OkaeriSerdesPack;
 import eu.okaeri.configs.serdes.SerdesRegistry;
 import net.kyori.adventure.text.Component;
-import net.kyori.adventure.text.serializer.ComponentSerializer;
 import org.jetbrains.annotations.NotNull;
 import org.mythicprojects.yetanothermessageslibrary.adventure.GlobalAdventureSerializer;
 import org.mythicprojects.yetanothermessageslibrary.config.serdes.holder.ActionBarHolderTransformer;
@@ -14,9 +13,9 @@ import org.mythicprojects.yetanothermessageslibrary.config.serdes.holder.TitleHo
 
 public class YAMLSerdes implements OkaeriSerdesPack {
 
-    private final ComponentSerializer<Component, ? extends Component, String> componentSerializer;
+    private final net.kyori.adventure.text.serializer.ComponentSerializer<Component, ? extends Component, String> componentSerializer;
 
-    public YAMLSerdes(@NotNull ComponentSerializer<Component, ? extends Component, String> componentSerializer) {
+    public YAMLSerdes(@NotNull net.kyori.adventure.text.serializer.ComponentSerializer<Component, ? extends Component, String> componentSerializer) {
         this.componentSerializer = componentSerializer;
     }
 
@@ -35,7 +34,7 @@ public class YAMLSerdes implements OkaeriSerdesPack {
         registry.register(new SendableMessageSerializer());
 
         // Utilities
-        registry.register(new ComponentTransformer(this.componentSerializer));
+        registry.register(new ComponentSerializer(this.componentSerializer));
         registry.register(new KeyTransformer());
     }
 
